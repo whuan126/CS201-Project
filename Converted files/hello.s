53150aa4a1adec700cb6b6725163ed3b6e0e7239
unknown <siddheshjadhav2001@gmail.com>
2024-11-04 17:09:38 -0800

    Added test files

---
+++ Converted files/hello.s
@@ -0,0 +1,40 @@
+	.text
+	.def	 @feat.00;
+	.scl	3;
+	.type	0;
+	.endef
+	.globl	@feat.00
+.set @feat.00, 0
+	.file	"hello.c"
+	.def	 main;
+	.scl	2;
+	.type	32;
+	.endef
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+main:                                   # @main
+.seh_proc main
+# %bb.0:
+	pushq	%rbp
+	.seh_pushreg %rbp
+	subq	$48, %rsp
+	.seh_stackalloc 48
+	leaq	48(%rsp), %rbp
+	.seh_setframe %rbp, 48
+	.seh_endprologue
+	callq	__main
+	movl	$0, -4(%rbp)
+	leaq	.L.str(%rip), %rcx
+	callq	printf
+	xorl	%eax, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	retq
+	.seh_endproc
+                                        # -- End function
+	.section	.rdata,"dr"
+.L.str:                                 # @.str
+	.asciz	"Hello, LLVM!\n"
+
+	.addrsig
+	.addrsig_sym printf
